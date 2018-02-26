e170201d36755d2c01f691e6b57a7579058483ff
babakpst <babakp@utexas.edu>
2018-02-26 10:56:55 -0600

    Adding Discretization_1D sub

--- src/Discretization_mod.f90
+++ src/Discretization_mod.f90
@@ -22,31 +22,134 @@
 !
 ! ================================ L O C A L   V A R I A B L E S ==================================
 ! (Refer to the main code to see the list of imported variables)
-!  . . . . . . . . . . . . . . . . Variables . . . . . . . . . . . . . . . . . . . . . . . . . . .
+!  . . . . . . . . . . . . . . . . variables . . . . . . . . . . . . . . . . . . . . . . . . . . .
 !
 !##################################################################################################
 
-Module Discretization_mod
+module Discretization_mod
 
 ! Libraries =======================================================================================
 
 ! User defined modules ============================================================================
 use Parameters_mod
 
-Implicit None
+implicit none
 
-Public
-Private
+!Public
+!Private
 
-  Interface Discretize
-    Module Procedure Discretize_1D
-  End Interface
-
-
-Contains
+  interface Discretize
+    module procedure Discretize_1D
+  end interface
 
 
+contains
+
+
+!##################################################################################################
+! Purpose: This subroutine discretizes the 1D domain and creates the required arrays.
+!
+! Developed by: Babak Poursartip
+! Supervised by: Clint Dawson
+!
+! The Institute for Computational Engineering and Sciences (ICES)
+! The University of Texas at Austin
+!
+! ================================ V E R S I O N ==================================================
+! V0.00: 02/26/2018 - File initiated.
+! V0.01: 00/00/2018 - Initiated: Compiled without error for the first time.
+!
+! File version $Id $
+!
+! Last update: 02/26/2018
+!
+! ================================ L O C A L   V A R I A B L E S ==================================
+! (Refer to the main code to see the list of imported variables)
+!  . . . . . . . . . . . . . . . . variables . . . . . . . . . . . . . . . . . . . . . . . . . . .
+!
+!##################################################################################################
+
+subroutine Discretize_1D(                                             &
+!                                                                     & ! integer (1) variables
+!                                                                     & ! integer (2) variables
+!                                                                     & ! integer (4) variables
+!                                                                     & ! integer (8) variables
+!                                                                     & ! real variables
+!                                                                     & ! integer arrays
+!                                                                     & ! real arrays
+!                                                                     & ! characters
+!                                                                     & ! type
+)
+
+
+! Libraries =======================================================================================
+
+! User defined modules ============================================================================
+
+
+implicit none
+
+! Global variables ================================================================================
+
+! - integer variables -----------------------------------------------------------------------------
+!#integer (kind=Shrt), intent(in)    ::
+!#integer (kind=Shrt), intent(inout) ::
+!#integer (kind=Shrt), intent(out)   ::
+! - real variables --------------------------------------------------------------------------------
+!#real (kind=Dbl),     intent(in)    ::
+!#real (kind=Dbl),     intent(inout) ::
+!#real (kind=Dbl),     intent(out)   ::
+! - complex variables -----------------------------------------------------------------------------
+!#complex,             intent(in)    ::
+!#complex,             intent(inout) ::
+!#complex,             intent(out)   ::
+! - integer Arrays --------------------------------------------------------------------------------
+!#integer (kind=Shrt), intent(in),    dimension (:  )  ::
+!#integer (kind=Shrt), intent(in),    dimension (:,:)  ::
+!#integer (kind=Shrt), intent(in)    ::
+!#integer (kind=Shrt), intent(inout) ::
+!#integer (kind=Shrt), intent(out)   ::
+! - real Arrays -----------------------------------------------------------------------------------
+!#real (kind=Dbl),     intent(in),    dimension (:  )  ::
+!#real (kind=Dbl),     intent(inout), dimension (:  )  ::
+!#real (kind=Dbl),     intent(out),   dimension (:  )  ::
+! - character variables ---------------------------------------------------------------------------
+!#character (kind = ?, Len = ? ) ::
+! - logical variables -----------------------------------------------------------------------------
+!#logical   ::
+! - types -----------------------------------------------------------------------------------------
+!#type() ::
+
+
+! Local variables =================================================================================
+! - integer variables -----------------------------------------------------------------------------
+!#integer (kind=Shrt)  ::
+! - real variables --------------------------------------------------------------------------------
+!#real (kind=Dbl)      ::
+! - complex variables -----------------------------------------------------------------------------
+!#complex              ::
+! - integer Arrays --------------------------------------------------------------------------------
+!#integer (kind=Shrt), dimension (:)  ::
+!#integer (kind=Shrt), Allocatable, dimension (:)  ::
+! - real Arrays -----------------------------------------------------------------------------------
+!#real (kind=Dbl), dimension (:)      ::
+!#real (kind=Dbl), allocatable, dimension (:)  ::
+! - character variables ---------------------------------------------------------------------------
+!#character (kind = ?, Len = ? ) ::
+! - logical variables -----------------------------------------------------------------------------
+!#logical   ::
+! - type ------------------------------------------------------------------------------------------
+
+! code ============================================================================================
+write(*,       *) " subroutine <Discretize_1D>: "
+write(FileInfo,*) " subroutine <Discretize_1D>: "
 
 
 
-End Module Discretization_mod
+write(*,       *) " end subroutine <Discretize_1D>"
+write(FileInfo,*) " end subroutine <Discretize_1D>"
+return
+end subroutine Discretize_1D
+
+
+end module Discretization_mod
