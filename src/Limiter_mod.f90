40324deeab37f5160dca6a25e3e332877320fb30
babakpst <babakp@utexas.edu>
2018-03-20 09:01:24 -0500

    add limiter

--- src/Limiter_mod.f90
+++ src/Limiter_mod.f90
@@ -23,6 +23,7 @@
 ! Impose_Boundary_Condition_1D_sub: Imposes boundary conditions on the 1D model.
 ! Limiters_sub: Contains various limiters.
 ! Jacobian_sub: computes the Jacobian matrix, Jacobian plus, and Jacobian minus at each cell.
+! Eigenvalues_sub: computes the eigenvalues of a 2x2 matrix.
 !
 ! ================================ F U N C T I O N ================================================
 !
@@ -729,6 +730,114 @@ write(FileInfo,*) " end subroutine < Jacobian_sub >"
 return
 end subroutine Jacobian_sub
 
+
+!##################################################################################################
+! Purpose: This subroutine computes the eiqenvalues of a 2x2 matrix.
+!
+! Developed by: Babak Poursartip
+! Supervised by:
+!
+! The Institute for Computational Engineering and Sciences (ICES)
+! The University of Texas at Austin
+!
+! ================================ V E R S I O N ==================================================
+! V0.00: 03/19/2018 - Subroutine initiated.
+! V0.01: 03/19/2018 - Initiated: Compiled without error for the first time.
+!
+! File version $Id $
+!
+! Last update: 03/19/2018
+!
+! ================================ L O C A L   V A R I A B L E S ==================================
+! (Refer to the main code to see the list of imported variables)
+!  . . . . . . . . . . . . . . . . Variables . . . . . . . . . . . . . . . . . . . . . . . . . . .
+!
+!##################################################################################################
+
+subroutine Eigenvalues_sub(                                           &
+!                                                                     & ! integer (1) variables
+!                                                                     & ! integer (2) variables
+!                                                                     & ! integer (4) variables
+!                                                                     & ! integer (8) variables
+!                                                                     & ! real variables
+!                                                                     & ! integer arrays
+!                                                                     & ! real arrays
+!                                                                     & ! characters
+!                                                                     & ! type
+)
+
+
+! Libraries =======================================================================================
+
+! User defined modules ============================================================================
+
+
+implicit none
+
+! Global variables ================================================================================
+
+! - integer variables -----------------------------------------------------------------------------
+!#integer(kind=Shrt), intent(in)    ::
+!#integer(kind=Shrt), intent(inout) ::
+!#integer(kind=Shrt), intent(out)   ::
+! - real variables --------------------------------------------------------------------------------
+!#real(kind=Dbl),     intent(in)    ::
+!#real(kind=Dbl),     intent(inout) ::
+!#real(kind=Dbl),     intent(out)   ::
+! - complex variables -----------------------------------------------------------------------------
+!#complex,             intent(in)    ::
+!#complex,             intent(inout) ::
+!#complex,             intent(out)   ::
+! - integer Arrays --------------------------------------------------------------------------------
+!#integer(kind=Shrt), intent(in),    dimension (:  )  ::
+!#integer(kind=Shrt), intent(in),    dimension (:,:)  ::
+!#integer(kind=Shrt), intent(in)    ::
+!#integer(kind=Shrt), intent(inout) ::
+!#integer(kind=Shrt), intent(out)   ::
+! - real Arrays -----------------------------------------------------------------------------------
+!#real(kind=Dbl),     intent(in),    dimension (:  )  ::
+!#real(kind=Dbl),     intent(inout), dimension (:  )  ::
+!#real(kind=Dbl),     intent(out),   dimension (:  )  ::
+! - character variables ---------------------------------------------------------------------------
+!#character(kind = ?, Len = ? ) ::
+! - logical variables -----------------------------------------------------------------------------
+!#logical   ::
+! - types -----------------------------------------------------------------------------------------
+!#type() ::
+
+
+! Local variables =================================================================================
+! - integer variables -----------------------------------------------------------------------------
+!#integer(kind=Shrt)  ::
+! - real variables --------------------------------------------------------------------------------
+!#real(kind=Dbl)      ::
+! - complex variables -----------------------------------------------------------------------------
+!#complex              ::
+! - integer Arrays --------------------------------------------------------------------------------
+!#integer(kind=Shrt), dimension (:)  ::
+!#integer(kind=Shrt), Allocatable, dimension (:)  ::
+! - real Arrays -----------------------------------------------------------------------------------
+!#real(kind=Dbl), dimension (:)      ::
+!#real(kind=Dbl), allocatable, dimension (:)  ::
+! - character variables ---------------------------------------------------------------------------
+!#character(kind = ?, Len = ? ) ::
+! - logical variables -----------------------------------------------------------------------------
+!#logical   ::
+! - type ------------------------------------------------------------------------------------------
+
+! code ============================================================================================
+write(*,       *) " subroutine < Eigenvalues_sub >: "
+write(FileInfo,*) " subroutine < Eigenvalues_sub >: "
+
+
+
+
+
+
+write(*,       *) " end subroutine < Eigenvalues_sub >"
+write(FileInfo,*) " end subroutine < Eigenvalues_sub >"
+return
+end subroutine Eigenvalues_sub
 
 end module LaxWendroff_with_limiter_mod
 
